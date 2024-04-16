f45faf6c3c8d851d6e604cfc2ec43ef686865a51
taruma sakti <hi@taruma.info>
2024-04-17 06:32:07 +0700

    Refactor run_completeness.py to improve code organization and readability

--- run_completeness.py
+++ run_completeness.py
@@ -1,11 +1,26 @@
 """SCRIPT TO GENERATE COMPLETENESS TABLE"""
-from pyconfig import appConfig
+
 from pathlib import Path
 import pandas as pd
 import pyfunc
+from pyconfig import appConfig
+
+# pylint: disable=invalid-name
 
 
-def flat_multiindex_monthly(dataframe: pd.DataFrame) -> pd.DataFrame:
+def flatten_multiindex_monthly(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Flatten the multi-index of a DataFrame by
+        renaming the index levels and converting it to a single-level index
+        with a date range as the index values.
+
+    Args:
+        dataframe (pd.DataFrame): The DataFrame with a multi-index to be flattened.
+
+    Returns:
+        pd.DataFrame: The DataFrame with a flattened index, where the index values are a date range.
+
+    """
     table = dataframe.copy()
     table.index = table.index.rename(["tahun", "bulan"])
     flat_index = table.index.to_flat_index()
@@ -20,18 +35,28 @@ def flat_multiindex_monthly(dataframe: pd.DataFrame) -> pd.DataFrame:
     return table
 
 
-def percent_count_monthly(dataframe: pd.DataFrame) -> pd.DataFrame:
+def calculate_monthly_completeness(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the monthly completeness of a given dataframe.
+
+    Parameters:
+    dataframe (pd.DataFrame): The input dataframe.
+
+    Returns:
+    pd.DataFrame: A dataframe containing the monthly completeness percentages.
+    """
     table = dataframe.copy()
     monthly_count = table.groupby(by=[table.index.year, table.index.month]).count()
     monthly_len = (
         table.groupby(by=[table.index.year, table.index.month]).size().to_frame()
     )
-    monthly_count = flat_multiindex_monthly(monthly_count)
+    monthly_count = flatten_multiindex_monthly(monthly_count)
     monthly_percent = monthly_count.div(monthly_len.values, axis="columns")
     return monthly_percent.rename_axis("date")
 
 
 def main():
+    """Main function to generate the completeness table."""
     FOLDER_RAINFALL = Path(appConfig.DATASET.RAINFALL)
     FOLDER_COMPLETENESS = Path(appConfig.DATASET.COMPLETENESS)
 
@@ -57,7 +82,7 @@ def main():
                 key = metadata_stations.loc[stat_id, "key"]
                 _df_rainfall = read_rr.get(key)
                 pyfunc.replace_unmeasured_data(_df_rainfall)
-                _df_completeness = percent_count_monthly(_df_rainfall)
+                _df_completeness = calculate_monthly_completeness(_df_rainfall)
                 _df_completeness = _df_completeness.round(3) * 100
                 store_comp.put(key, value=_df_completeness)
 
